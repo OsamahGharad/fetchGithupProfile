79872654f8806f2ab7a8e57e6a5729fc413a62a4
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-31 23:28:03 +0300

    done

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
     const profile_img =document.getElementById('img');
     const repos =document.getElementById('repos');
     const link =document.getElementById('link');
+    const repoName =document.getElementById('repoName');
+    const repoLang =document.getElementById('repoLang');
+
     // const button =document.getElementById('getProfile');
     
     const fetchProfile =() =>{
@@ -12,7 +15,32 @@
         fetch(`https://api.github.com/users/OsamahGharad`).then((res)=>res.json()).then((data) =>displayProfile(data));
         
     }
+    const fetchRepos =( )=>{
+        fetch(`https://api.github.com/users/OsamahGharad/repos`)
+        .then((res) =>res.json())
+        .then((data) => {
+            for(let i=0 ; i<29 ; i++){
+                (displayRep(data[i],i+1)) 
+              }
+        })
+        
+      
+    }
   
+    const displayRep =(data,number)=>{
+           const repoCard=document.createElement('div');
+           repoCard.classList.add('reop_list');
+           const repoNumber=document.createElement('p');
+           const repoName=document.createElement('p');
+           const repoLang=document.createElement('p');
+           repoNumber.innerText=`${number}`;
+           repoName.innerText=`${data.name}`;
+           repoLang.innerText=`${data.language}`;
+           repoCard.appendChild(repoNumber);
+           repoCard.appendChild(repoName);
+           repoCard.appendChild(repoLang);
+           repos.appendChild(repoCard);
+    }
 
     const displayProfile= (data) =>{
       
@@ -20,9 +48,12 @@
              profile_img.src=`${data.avatar_url}`;
             //  link.innerText=`URL : ${data.html_url}`;
              link.innerHTML=`<span>URL :</span> <a href=" ${data.html_url}">${data.html_url}</a>`;
-            //  repos.innerHTML= `<span>Repos :</span>${data.repos_url}`
-            repos.innerHTML= `<span>Repos :</span> `
+                  //  repos.innerHTML= `<span>Repos :</span>${data.repos_url}`
+          
         }
 
-window.addEventListener('DOMContentLoaded',fetchProfile)
+window.addEventListener('DOMContentLoaded',(event)=>{
+    fetchProfile();
+    fetchRepos();
+})
     
